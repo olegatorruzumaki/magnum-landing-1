729bfc588bb1c81f9bbfcaf3f3dfa54e3e9f53da
Oleg <olegkurbatov97@mail.ru>
2021-10-06 15:39:23 +0600

    base- 2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app" class="container">
-    <router-view class="row center-screen py-3"/>
+  <div id="app" class="container-fluid">
+    <router-view class="row"/>
   </div>
 </template>
 
@@ -11,17 +11,16 @@ export default {
   name: 'App',
   components: {},
   data() {
-    this.$i18n.locale = this.$route.query.lang || 'ru';
+    this.$i18n.locale = 'RU';
     return {}
   },
   watch: {
     $route(to, from) {
-      this.$i18n.locale = to.query.lang || 'ru';
       console.log('from', from);
     }
   },
   mounted() {
-    console.log('app');
+    console.log('app', this.$i18n.locale);
   },
   methods: {}
 }
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="main">
-    test shit
+    <div class="main__base px-0">
+      <div class="header col-12 d-inline-flex justify-content-between">
+        <a href="#"><img src="../assets/logo.svg" alt=""></a>
+        <div class="d-inline-flex main__lang">
+          <div class="cursor-pointer test1" :class="{selected: $i18n.locale === 'RU'}" @click="changeLocale('RU')">RU
+          </div>
+          <div class="cursor-pointer test2" :class="{selected: $i18n.locale === 'UZ'}" @click="changeLocale('UZ')">UZ
+          </div>
+          <div class="cursor-pointer test3" :class="{selected: $i18n.locale === 'EN'}" @click="changeLocale('EN')">EN
+          </div>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="main__title position-relative">
+          <div class="main__title-1" v-html="$t('bgTitle-1')">ОТКРЫВАЕМ</div>
+          <div class="main__title-2 position-absolute">
+            <img :src="require(`@/assets/cart.png`)" id='cart' alt="cart">
+          </div>
+          <div class="main__title-3" v-html="$t('bgTitle-2')">СЕКРЕТ</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,7 +37,11 @@ export default {
   mounted() {
     console.log('Main');
   },
-  methods: {}
+  methods: {
+    changeLocale(value) {
+      this.$i18n.locale = value;
+    }
+  }
 }
 
 </script>
